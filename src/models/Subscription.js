32c8fcca8fe2cef45a2acb456b0688a54ca9921a
viz-nu <vishnu.teja101.vt@gmail.com>
2025-02-28 19:52:13 +0530

    fix: users issue

--- src/models/Subscription.js
+++ src/models/Subscription.js
@@ -2,8 +2,8 @@ import { model, Schema } from 'mongoose';
 
 const SubscriptionsSchema = new Schema({
     name: String,
-    createdBy: { type: Schema.Types.ObjectId, ref: 'User' }
-},{
+    createdBy: { type: Schema.Types.ObjectId, ref: 'Users' }
+}, {
     timestamps: true
 });
-export const Subscriptions = model('Subscription', SubscriptionsSchema,"Subscriptions");
+export const Subscriptions = model('Subscription', SubscriptionsSchema, "Subscriptions");
