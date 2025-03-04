c681e90f19ac29b06d3f4ed37c7602b03f6a4e18
viz-nu <vishnu.teja101.vt@gmail.com>
2025-03-04 15:32:01 +0530

    fix: edit-business

--- src/controllers/admin/index.js
+++ src/controllers/admin/index.js
@@ -1,13 +1,21 @@
 import { errorWrapper } from "../../middleware/errorWrapper.js";
 import { Business } from "../../models/Business.js";
 export const Dashboard = errorWrapper(async (req, res) => {
-
     const business = await Business.findById(req.user.business).populate("agents collections members documents");
     if (!business) return { statusCode: 404, message: "Business not found", data: null }
-    return {
-        statusCode: 200, message: "Dashboard retrieved", data: {
-            user: req.user,
-            business: business
-        }
-    };
+    return { statusCode: 200, message: "Dashboard retrieved", data: { user: req.user, business: business } };
 })
+export const editBusiness = errorWrapper(async (req, res) => {
+    let business = await Business.findById(req.user.business)
+    if (!business) return { statusCode: 404, message: "Business not found", data: null }
+    const { logoURL, facts, sector, tagline, address, description, contact } = req.body
+    if (logoURL) business.logoURL = logoURL;
+    if (facts) business.facts = facts;
+    if (sector) business.sector = sector;
+    if (tagline) business.tagline = tagline;
+    if (address) business.address = address;
+    if (description) business.description = description;
+    if (contact) business.contact = contact;
+    await business.save();
+    return { statusCode: 200, message: "Business updated", data: business }
+});
--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { authMiddleware, isAdmin } from "../middleware/auth.js";
-import { Dashboard } from "../controllers/admin/index.js";
+import { Dashboard, editBusiness } from "../controllers/admin/index.js";
 
 export const AdminRouter = Router();
 //        {{localhost:5000}}/api/v1/auth/team-login
 // router.post("/team-login", TeamLogin)
 
 AdminRouter.get('/dashboard', authMiddleware, isAdmin, Dashboard);
+AdminRouter.put('/edit-business', authMiddleware, isAdmin, editBusiness);
