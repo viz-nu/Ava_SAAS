e7a6343912c04136df9fc4cbd2bd05d4b56643b1
viz-nu <vishnu.teja101.vt@gmail.com>
2025-03-05 22:09:09 +0530

    fix: Actions

--- src/models/Action.js
+++ src/models/Action.js
@@ -1,6 +1,8 @@
 import { model, Schema } from 'mongoose';
 
 const ActionSchema = new Schema({
+    name: String,
+    webhook: Object,
     business: { type: Schema.Types.ObjectId, ref: 'Businesses' },
     intent: String,
     intentData: Object
