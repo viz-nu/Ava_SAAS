9b17924183d95b41ce079457987ec214cd969385
viz-nu <vishnu.teja101.vt@gmail.com>
2025-03-17 16:42:51 +0530

    fix: creating actions

--- src/models/Action.js
+++ src/models/Action.js
@@ -4,6 +4,7 @@ const ActionSchema = new Schema({
     name: String,
     business: { type: Schema.Types.ObjectId, ref: 'Businesses' },
     intent: String,
+    intentType:{ type: String, enum: ["Query", "Response","Conversation"] },
     dataSchema: Object,
     accessType: { type: String, enum: ["Public", "Private"] },
     auth: { endpoint: String, dataSchema: Object },
