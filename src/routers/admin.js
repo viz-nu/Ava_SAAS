b7a03b28dff42c2a887193f2d591bd4a37163cf1
viz-nu <vishnu.teja101.vt@gmail.com>
2025-03-17 16:43:00 +0530

    feat: dashboard

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { authMiddleware, isAdmin } from "../middleware/auth.js";
-import { createActions, Dashboard, deleteAction, editBusiness, getActionById, getActions, raiseTicket, updateAction } from "../controllers/admin/index.js";
+import { createActions, Dashboard, deleteAction, DetailedAnalysis, editBusiness, getActionById, getActions, raiseTicket, updateAction } from "../controllers/admin/index.js";
 
 export const AdminRouter = Router();
 //        {{localhost:5000}}/api/v1/auth/team-login
 // router.post("/team-login", TeamLogin)
 
 AdminRouter.get('/dashboard', authMiddleware, isAdmin, Dashboard);
+AdminRouter.post('/query-analysis', authMiddleware, isAdmin, DetailedAnalysis);
 AdminRouter.put('/edit-business', authMiddleware, isAdmin, editBusiness);
 AdminRouter.post('/raise-ticket', authMiddleware, isAdmin, raiseTicket);
 AdminRouter.post('/actions', authMiddleware, isAdmin, createActions);
