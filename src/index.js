66b903b0d1c9b1a95eb0112545b1a5fad8f80e70
viz-nu <vishnu.teja101.vt@gmail.com>
2025-03-18 00:13:03 +0530

    fix: submit request

--- src/index.js
+++ src/index.js
@@ -160,8 +160,9 @@ app.post('/v2/chat-bot', async (req, res) => {
 });
 app.post("/trigger", async (req, res) => {
     try {
-        const { actionId, dataSchema } = req.body
-
+        // const { } = req.body
+        console.log(req.body);
+        res.status(200).json({ success: true, message: "received submit request", data: req.body })
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "internal server error", error: err.message });
