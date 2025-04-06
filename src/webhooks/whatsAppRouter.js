da1bc524666df2e03eb17d2e5c686f18af495c24
viz-nu <vishnu.teja101.vt@gmail.com>
2025-04-06 23:44:36 +0530

    fix: webhook

--- src/webhooks/whatsAppRouter.js
+++ src/webhooks/whatsAppRouter.js
@@ -1,21 +1,21 @@
 import { Router } from "express";
+import { parse } from "url";
 export const whatsappRouter = Router()
-whatsappRouter.get('/:phoneNumber', async (req, res) => {
+whatsappRouter.get('/:params', async (req, res) => {
     try {
-        const mode = req.query['hub.mode'];
-        const token = req.query['hub.verify_token'];
-        const challenge = req.query['hub.challenge'];
-
+        const parsedUrl = parse(req.originalUrl, true);
+        const query = parsedUrl.query;
+        const mode = query['hub.mode'];
+        const token = query['hub.verify_token'];
+        const challenge = query['hub.challenge'];
         console.log("Mode:", mode);
         console.log("Verify Token:", token);
         console.log("Challenge:", challenge);
         console.log("Params:", req.params);
-        console.log("Query:", req.query);
-        console.log("Body:", req.body);
-
+        console.log("Query:", query);
         // Optional: Verify the token before responding
-        const VERIFY_TOKEN = process.env.META_VERIFICATION_TOKEN;
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        if (mode === 'subscribe' && token === process.env.META_VERIFICATION_TOKEN) {
+            console.log("WEBHOOK_VERIFIED");
             return res.status(200).send(challenge);
         } else {
             return res.sendStatus(403);
@@ -27,14 +27,15 @@ whatsappRouter.get('/:phoneNumber', async (req, res) => {
 });
 whatsappRouter.post('/:params', async (req, res) => {
     try {
-        
+
         // const mode = req.query['hub.mode'];
         // const token = req.query['hub.verify_token'];
         // const challenge = req.query['hub.challenge'];
         // console.log({ mode, token, challenge });
-
+        const parsedUrl = parse(req.originalUrl, true);
+        const query = parsedUrl.query;
         console.log("params", req.params);
-        console.log("QUERY", req.query);
+        console.log("QUERY", query);
         console.log("body", req.body)
         return res.status(200);
     } catch (error) {
