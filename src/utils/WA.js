a8eb9f009eb3bbc6b022b22fb0087eedcc05e362
viz-nu <vishnu.teja101.vt@gmail.com>
2025-04-08 01:34:41 +0530

    fix: sending messages

--- src/utils/WA.js
+++ src/utils/WA.js
@@ -1,38 +1,44 @@
 import axios from "axios"
-
-export const sendWAMessage = async ({ to, type = "text", Data = {} }) => {
+export const sendWAMessage = async ({ phone_number_id = "613445751852844", messaging_product = "whatsapp", to, type = "text", Data }) => {
     try {
 
-        const whatsappApiUrl = 'https://graph.facebook.com/v20.0/phone_number_id/messages';
+        const whatsappApiUrl = `https://graph.facebook.com/v20.0/${phone_number_id}/messages`;
         const token = process.env.AVAKADO_WABA_TOKEN;
         // Default payload structure
-        let payload = { messaging_product: "whatsapp", recipient_type: "individual", to: to };
+        let payload = { messaging_product, recipient_type: "individual", to: to };
         // Handle different message types
         if (type === "text") {
             payload.type = "text";
-            payload.text = { body: Data.text };
+            payload.text = Data;
         } else if (type === "image") {
             payload.type = "image";
-            payload.image = { link: Data.url };
+            payload.image = Data;
         } else if (type === "audio") {
             payload.type = "audio";
-            payload.audio = { link: Data.url };
+            payload.audio = Data;
         } else if (type === "document") {
             payload.type = "document";
-            payload.document = { link: Data.url, caption: Data.caption || "" };
+            payload.document = Data;
         }
         const { data } = await axios.post(whatsappApiUrl, payload, { headers: { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' } })
-        console.log("✅ Message sent successfully:", responseData);
+        console.log("✅ Message sent successfully:", data);
         return data
     } catch (error) {
-        console.error("❌ Error sending WhatsApp message:", error);
+        console.error("❌ Error sending WhatsApp message:");
+        if (error.response) {
+            // The request was made, server responded with a status code outside 2xx
+            console.error('Error Response:');
+            console.error('Status:', error.response.status);
+            console.error('Data:', error.response.data);
+            console.error('Headers:', error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response received
+            console.error('No Response received:', error.request);
+        } else {
+            // Something happened in setting up the request
+            console.error('Axios Error Message:', error.message);
+        }
+        console.error('Config:', error.config);
         return null
     }
 }
-// sendTemplateMessage()
-
-// sendTextMessage()
-
-// sendMediaMessage()
-
-// uploadImage()
