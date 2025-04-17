8cfb8c184d3659217ba8c32f72e697fe8e47e134
viz-nu <vishnu.teja101.vt@gmail.com>
2025-04-17 17:34:05 +0530

    fix: telegram id address

--- src/utils/telegraf.js
+++ src/utils/telegraf.js
@@ -2,8 +2,8 @@ import { Agent } from "../models/Agent.js";
 
 export const getBotDetails = async (botId) => {
     try {
-        const agent = await Agent.findOne({ "personalInfo.telegram.id": botId })
-        return agent
+        const agent = await Agent.findOne({ "agent.integrations.telegram.id": botId })
+        return agent.toObject()
     } catch (error) {
         console.log(error);
         return null
