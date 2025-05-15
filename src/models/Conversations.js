e3ae88678bb12ab41c683ff5df11ddcb70be55d0
viz-nu <vishnu.teja101.vt@gmail.com>
2025-05-15 12:55:09 +0530

    wa

--- src/models/Conversations.js
+++ src/models/Conversations.js
@@ -3,6 +3,7 @@ import { model, Schema } from 'mongoose';
 const ConversationSchema = new Schema({
     business: { type: Schema.Types.ObjectId, ref: 'Businesses' },
     telegramChatId: String,
+    whatsappChatId: String,
     contact: Object,
     agent: { type: Schema.Types.ObjectId, ref: 'Agent' },
     analysis: { type: Schema.Types.Mixed },
