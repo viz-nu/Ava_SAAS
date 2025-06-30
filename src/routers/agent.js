165e36e92aa013288df089d849848e2bc715499f
viz-nu <vishnu.teja101.vt@gmail.com>
2025-06-30 18:50:55 +0530

    misc

--- src/routers/agent.js
+++ src/routers/agent.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authMiddleware, isAdmin } from "../middleware/auth.js";
-import { createAgent, deleteAgent, getAgentById, getAllAgents, integrations, promptGenerator, updateAgent } from "../controllers/agent/index.js";
+import { createAgent, deleteAgent, getAgentById, getAllAgents, promptGenerator, updateAgent } from "../controllers/agent/index.js";
 
 export const AgentsRouter = Router()
 AgentsRouter.post('/', authMiddleware, isAdmin, createAgent);
@@ -9,4 +9,3 @@ AgentsRouter.get('/', authMiddleware, isAdmin, getAllAgents);
 AgentsRouter.get('/:id', authMiddleware, isAdmin, getAgentById);
 AgentsRouter.put('/:id', authMiddleware, isAdmin, updateAgent);
 AgentsRouter.delete('/:id', authMiddleware, isAdmin, deleteAgent);
-AgentsRouter.post('/integration/:id', authMiddleware, isAdmin, integrations);
