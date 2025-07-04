6f8d7457268941e0b37e96c54a84e3c92b9decce
viz-nu <vishnu.teja101.vt@gmail.com>
2025-07-05 00:37:17 +0530

    fix: parameter

--- src/models/Action.js
+++ src/models/Action.js
@@ -1,26 +1,4 @@
 import { model, Schema } from 'mongoose';
-const ToolParameterSchema = new Schema({
-    dataType: {
-        type: String,
-        enum: ["object", "string", "number", "boolean", "integer", "array", "null"],
-        required: true
-    },
-    dataFormat: Schema.Types.Mixed,
-    isRequired: { type: Boolean, required: true },
-    key: { type: String, required: true },
-    validation: String,
-    description: String,
-    properties: {
-        type: Map,
-        of: new Schema({
-            type: String,
-            description: String,
-        }, { _id: false }),
-        default: undefined
-    },
-    additionalProperties: Boolean,
-    label: String,
-}, { _id: false });
 const ActionSchema = new Schema({
     name: String,
     business: { type: Schema.Types.ObjectId, ref: 'Businesses' },
@@ -28,7 +6,7 @@ const ActionSchema = new Schema({
     name: String,
     description: String,
     needsApproval: Boolean, // Knowledge fetching doesn't need approval
-    parameters: ToolParameterSchema,
+    parameters: Schema.Types.Mixed,
     functionString: String,
     errorFunction: String,
     UI: Schema.Types.Mixed
