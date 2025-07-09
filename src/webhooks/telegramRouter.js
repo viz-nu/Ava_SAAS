b4ccdff58810d8bac8133fa82502aca36367ab72
viz-nu <vishnu.teja101.vt@gmail.com>
2025-07-09 16:23:08 +0530

    kjhbh

--- src/webhooks/telegramRouter.js
+++ src/webhooks/telegramRouter.js
@@ -18,6 +18,8 @@ telegramRouter.post('/:botId', async (req, res) => {
         const { latitude, longitude } = message.location || {};
         const { phone_number, first_name, user_id } = message.contact || {};
         const text = message.text || null;
+        console.log({ type: "telegram", botId });
+
         // Respond immediately to prevent retries
         res.status(200).json({ success: true });
         // Process asynchronously to avoid delays
