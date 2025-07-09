f4d22d33b33b5ef778ac99609c78fce28ac63301
viz-nu <vishnu.teja101.vt@gmail.com>
2025-07-09 16:41:05 +0530

    testing. webgook stting

--- src/controllers/channels/index.js
+++ src/controllers/channels/index.js
@@ -54,7 +54,8 @@ export const createChannel = errorWrapper(async (req, res) => {
             await channel.save()
             await channel.updateStatus("fetched bot details")
             try {
-                await bot.telegram.setWebhook(webhookUrl);
+                const resp = await bot.telegram.setWebhook(webhookUrl);
+                console.log({ resp })
             } catch (error) {
                 console.log(error);
                 return { statusCode: 500, message: "Internal Server Error While Setting Up Telegram Webhook", data: null };
