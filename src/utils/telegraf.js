5cd617b25e354ecfd6e450ff668fabc5017a0481
viz-nu <vishnu.teja101.vt@gmail.com>
2025-07-10 18:26:51 +0530

    dC

--- src/utils/telegraf.js
+++ src/utils/telegraf.js
@@ -7,8 +7,6 @@ export const getBotDetails = async ({ type, botId }) => {
             case "telegram":
                 const channelDetails = await Channel.findOne({ "config.id": botId })
                 const agentDetails = await AgentModel.findOne({ channels: channelDetails._id }).populate("business actions")
-                console.log({ agentDetails, channelDetails });
-                
                 return { agentDetails, channelDetails }
             default:
                 break;
