18ea4d5bf27f4a85a027bb99efd3c3613488bf3c
viz-nu <vishnu.teja101.vt@gmail.com>
2025-07-28 16:09:05 +0530

    jksdgf

--- src/models/Conversations.js
+++ src/models/Conversations.js
@@ -1,5 +1,5 @@
 import { model, Schema } from 'mongoose';
-const ConversationStatusEnum = ["initiated", "active", "interrupted", "inactive"]
+const ConversationStatusEnum = ["initiated", "active", "interrupted", "inactive", "disconnected"];
 const ConversationSchema = new Schema({
     business: { type: Schema.Types.ObjectId, ref: 'Businesses' },
     channel: String,
@@ -8,6 +8,10 @@ const ConversationSchema = new Schema({
     contact: Schema.Types.Mixed,
     agent: { type: Schema.Types.ObjectId, ref: 'Agent' },
     status: { type: String, enum: ConversationStatusEnum, default: "initiated" },
+    sockets: {
+        socketId: String,
+        disconnectReason: String,
+    },
     /** Conversation state & runtime extras */
     session: Schema.Types.Mixed,
     state: String,
--- src/utils/io.js
+++ src/utils/io.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import { createClient } from "redis";
 import { createAdapter } from "@socket.io/redis-adapter";
+import { Conversation } from "../models/Conversations";
 
 let io;
 
@@ -17,6 +18,111 @@ export async function initializeSocket(server) {
             origin: "*",
             credentials: true,
         },
+    });
+    // Create a separate namespace for admin-related events
+    const adminNamespace = io.of('/admin');
+    adminNamespace.on('connection', (socket) => {
+
+        console.log('Admin connected:', socket.id);
+
+
+        socket.on('join-admin', (adminId) => {
+            console.log('Admin joined room:', adminId);
+            socket.join(adminId);
+        });
+
+        socket.on('trigger', async (triggerObject) => {
+            try {
+                const { action, data } = triggerObject;
+                console.log("Trigger action:", action, "with data:", data);
+                switch (action) {
+                    case 'getActiveUsersByAgent':
+                        const { agentId } = data;
+                        // get rooms for the agentId
+                        socket.emit('activeUsers', { count: (await ChatNameSpace.in(agentId).fetchSockets()).length });
+                        break;
+                    // case 'message':
+                    //     const { message, userId } = data;
+                    //     console.log("Message received:", message);
+                    //     if (userId) {
+                    //         io.to(userId).emit('message', { message, from: socket.id });
+                    //     }
+                    //     break;
+                    // case 'notification':
+                    //     const { notification, recipientId } = data;
+                    //     console.log("Notification sent to:", recipientId);
+                    //     if (recipientId) {
+                    //         io.to(recipientId).emit('notification', notification);
+                    //     }
+                    //     break;
+                    default:
+                        console.warn("Unknown action:", action);
+                }
+            }
+            catch (error) {
+                console.error("Error in trigger event:", error);
+            }
+        });
+        socket.on('disconnect', () => {
+            console.log('Admin disconnected:', socket.id);
+        });
+    });
+
+    // Create a namespace for chat
+    const ChatNameSpace = io.of('/chat');
+    ChatNameSpace.on('connection', (socket) => {
+        socket.on('join', async (triggerObject) => {
+            const conversationID = triggerObject.data.conversationID;
+            const agentId = triggerObject.data.agentId;
+            console.log("User joined conversation:", conversationID);
+            console.log(triggerObject);
+            if (conversationID) {
+                socket.join(conversationID);
+                await Conversation.updateOne({ _id: conversationID }, { $set: { socketId: socket.id, status: "active" } });
+            }
+            if (agentId) {
+                console.log("User joined agent room:", agentId);
+                socket.join(agentId);
+            }
+        });
+        socket.on('trigger', async (triggerObject) => {
+            try {
+                const { action, data } = triggerObject;
+                console.log("Trigger action:", action, "with data:", data);
+                switch (action) {
+                    case 'getActiveUsersByAgent':
+                        const { agentId } = data;
+                        // get rooms for the agentId
+                        const rooms = await ChatNameSpace.in(agentId).fetchSockets();
+
+                        break;
+                    // case 'message':
+                    //     const { message, userId } = data;
+                    //     console.log("Message received:", message);
+                    //     if (userId) {
+                    //         io.to(userId).emit('message', { message, from: socket.id });
+                    //     }
+                    //     break;
+                    // case 'notification':
+                    //     const { notification, recipientId } = data;
+                    //     console.log("Notification sent to:", recipientId);
+                    //     if (recipientId) {
+                    //         io.to(recipientId).emit('notification', notification);
+                    //     }
+                    //     break;
+                    default:
+                        console.warn("Unknown action:", action);
+                }
+            }
+            catch (error) {
+                console.error("Error in trigger event:", error);
+            }
+        });
+
+        socket.on('disconnect', async (reason) => {
+            console.log("User disconnected:", socket.id, "reason:", reason);
+            await Conversation.updateOne({ "sockets.socketId": socket.id }, { $set: { "disconnectReason": reason, status: "disconnected" } });
+        });
     });
 
     // Use Redis adapter
@@ -40,3 +146,38 @@ export async function initializeSocket(server) {
 }
 export { io };
 
+// {
+//     socketId: 'TZHDxLgrXERfhioeAAAB',
+//         namespace: '/chat',
+//             connected: true,
+//                 ip: '::ffff:127.0.0.1',
+//                     transport: 'websocket',
+//                         handshakeTime: 'Sun Jul 27 2025 00:39:26 GMT+0530 (India Standard Time)',
+//                             queryParams: [Object: null prototype] { EIO: '4', transport: 'websocket' },
+//     authData: { },
+//     headers: {
+//         'sec-websocket-version': '13',
+//             'sec-websocket-key': 'BxY8DAd6WNSd+4I5+95Kdg==',
+//                 connection: 'Upgrade',
+//                     upgrade: 'websocket',
+//                         'sec-websocket-extensions': 'permessage-deflate; client_max_window_bits',
+//                             host: 'localhost:3000'
+//     },
+//     userAgent: undefined,
+//         referer: undefined,
+//             rooms: ['TZHDxLgrXERfhioeAAAB']
+// }
+// console.log('user connected:', {
+//     socketId: socket.id,
+//     namespace: socket.nsp.name,
+//     connected: socket.connected,
+//     ip: socket.handshake.address || socket.conn.remoteAddress,
+//     transport: socket.conn.transport.name,
+//     handshakeTime: socket.handshake.time,
+//     queryParams: socket.handshake.query,
+//     authData: socket.handshake.auth,
+//     headers: socket.handshake.headers,
+//     userAgent: socket.handshake.headers['user-agent'],
+//     referer: socket.handshake.headers['referer'],
+//     rooms: [...socket.rooms], // Convert Set to array
+// });
