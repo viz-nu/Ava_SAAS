171d060028ef244a96adaf36ee7e4d86c3961364
viz-nu <vishnu.teja101.vt@gmail.com>
2025-07-31 19:27:57 +0530

    akjsdb

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authMiddleware, isAdmin } from "../middleware/auth.js";
-import { Analysis, Dashboard, DetailedAnalysis, editBusiness, newDashboard, raiseTicket } from "../controllers/admin/index.js";
+import { Dashboard, DetailedAnalysis, editBusiness, newDashboard, raiseTicket } from "../controllers/admin/index.js";
 
 export const AdminRouter = Router();
 //        {{localhost:5000}}/api/v1/auth/team-login
@@ -8,7 +8,6 @@ export const AdminRouter = Router();
 
 AdminRouter.get('/dashboard', authMiddleware, isAdmin, Dashboard);
 AdminRouter.get('/new-dashboard', authMiddleware, isAdmin, newDashboard);
-AdminRouter.post('/conversation-analysis', authMiddleware, isAdmin, Analysis);
 AdminRouter.post('/query-analysis', authMiddleware, isAdmin, DetailedAnalysis);
 AdminRouter.put('/edit-business', authMiddleware, isAdmin, editBusiness);
 AdminRouter.post('/raise-ticket', authMiddleware, isAdmin, raiseTicket);
