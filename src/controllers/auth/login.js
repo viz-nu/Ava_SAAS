ea51e07a8adb8d6a031fa81978465edb2ea1ef97
viz-nu <vishnu.teja101.vt@gmail.com>
2025-07-31 17:15:47 +0530

    cadsv

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -8,7 +8,7 @@ export const Login = errorWrapper(async (req, res) => {
     if (!user) return { statusCode: 401, message: "Invalid email" }
     if (!bcrypt.compareSync(password, user.password)) return { statusCode: 401, message: "Invalid password" }
     // if (!user.isVerified) return { statusCode: 403, message: "Email not confirmed. Please verify your email." }
-    const { newAccessToken, newRefreshToken } = await generateTokens(user._id, req.headers['user-agent'])
+    const { newAccessToken, newRefreshToken } = await generateTokens(user._id)
     res.cookie("AVA_RT", newRefreshToken, {
         secure: true,
         httpOnly: true,
@@ -24,7 +24,7 @@ export const superAdminLogin = errorWrapper(async (req, res) => {
     const user = await User.findOne({ email: email })
     if (!user) return { statusCode: 401, message: "Invalid email" }
     if (user.role !== "superAdmin") return { statusCode: 401, message: "Invalid Access Requested" }
-    const { newAccessToken, newRefreshToken } = await generateTokens(user._id, req.headers['user-agent'])
+    const { newAccessToken, newRefreshToken } = await generateTokens(user._id)
     res.cookie("AVA_RT", newRefreshToken, {
         secure: true,
         httpOnly: true,
