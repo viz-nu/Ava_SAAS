6d3e229738a1e86d79e5aded218b188003b87d7c
viz-nu <vishnu.teja101.vt@gmail.com>
2025-07-31 18:05:15 +0530

    fdab vdf

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { createApp } from './server.js';
 const PORT = process.env.PORT || 3000;
 const { app, server } = await createApp();
 server.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+const { ACCESS_SECRET, REFRESH_SECRET } = process.env
+console.log({ ACCESS_SECRET, REFRESH_SECRET, port: process.env.PORT, dburi: process.env.MONGO_URI });
 process.on('SIGINT', async () => {
   console.log('\n🛑 Gracefully shutting down...');
   try {
