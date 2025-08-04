282d2580e2254f2866066a686b5a11bb4da80f48
viz-nu <vishnu.teja101.vt@gmail.com>
2025-08-04 18:13:47 +0530

    ljkbrtkj5w

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,13 @@
 import { model, Schema } from 'mongoose';
-
+const ScopesEnum = [
+    
+]
 const UserSchema = new Schema({
     name: String,
     email: { type: String, unique: true },
     password: String,
     role: { type: String, enum: ['admin', 'manager', 'superAdmin'], default: 'admin' },
+    scopes: [{ type: String, enum: Object.keys(ScopesEnum) }],
     business: { type: Schema.Types.ObjectId, ref: 'Businesses' },
     isVerified: { type: Boolean, default: false },
     emailToken: String,
