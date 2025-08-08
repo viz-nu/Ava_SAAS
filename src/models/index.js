0e560ecbf857a20b6adb24a9958e38624ad2cae6
viz-nu <vishnu.teja101.vt@gmail.com>
2025-08-08 15:16:03 +0530

    .kjsadnv

--- src/models/index.js
+++
@@ -1,4 +0,0 @@
-import { User } from "./User.js"
-export const Models = {
-    User
-}
--- src/utils/tiktoken.js
+++
@@ -1,5 +0,0 @@
-import { encoding_for_model } from "tiktoken";
-export const tokenSize = (model, txt) => {
-    const enc = encoding_for_model(model);
-    return enc.encode(txt).length;
-}
