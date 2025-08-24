0d8c019fde2001d5a649579857248e24f7183970
viz-nu <vishnu.teja101.vt@gmail.com>
2025-08-24 23:00:35 +0530

    call/sms initiation

--- src/webhooks/twilioRouter.js
+++ src/webhooks/twilioRouter.js
@@ -20,44 +20,20 @@ twilioRouter.post('/sms/status', urlencoded({ extended: false }), async (req, re
         console.error("webhook middleware validation failed", error);
     }
 }, async (req, res) => {
-    try {
-        // console.log(JSON.stringify(req.body, null, 2));
-        // {
-        //     "ApiVersion": "2010-04-01",
-        //         "MessageStatus": "sent",
-        //             "SmsSid": "SM3eab196XXXXXXXXXXXXXeceaa93",
-        //                 "SmsStatus": "sent",
-        //                     "To": "+919DDDDD639",
-        //                         "From": "+198DDDDDD02",
-        //                             "MessageSid": "SM3eab196XXXXXXXXXXXXXeceaa93",
-        //                                 "AccountSid": "AC0ddXXXXXXXXXXXXXfa912"
-        // }
-        // {
-        //     "ApiVersion": "2010-04-01",
-        //         "MessageStatus": "delivered",
-        //             "RawDlrDoneDate": "2508242137",
-        //                 "SmsSid": "SM3eab196XXXXXXXXXXXXXeceaa93",
-        //                     "SmsStatus": "delivered",
-        //                         "To": "+919DDDDD639",
-        //                             "From": "+198DDDDDD02",
-        //                                 "MessageSid": "SM3eab196XXXXXXXXXXXXXeceaa93",
-        //                                     "AccountSid": "AC0ddXXXXXXXXXXXXXfa912"
-        // }
-        console.warn(`SMS SID: ${CallSid}, Status: ${CallStatus}`);
+    try {  
+        console.log("twilio sms status update", JSON.stringify(req.body, null, 2));
         res.status(200)
     } catch (error) {
-        console.log(error);
+        console.error(error);
         res.status(500).json({ msg: "internal server error" });
     }
 });
 twilioRouter.post('/call/status', urlencoded({ extended: false }), async (req, res) => {
     try {
-        console.log("twilio webhook body: ", JSON.stringify(req.body, null, 2));
-        // find in converation by CallSid as  voiceCallIdentifierNumberSID and update metadata.callDetails
-        console.warn(`CALL SID: ${CallSid}, Status: ${CallStatus}`);
+        console.log("twilio call status update", JSON.stringify(req.body, null, 2));
         res.status(200)
     } catch (error) {
-        console.log(error);
+        console.error(error);
         res.status(500).json({ msg: "internal server error" });
     }
 });
