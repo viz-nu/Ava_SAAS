34bda8741e96f115df5a41a923b32316cd15469e
viz-nu <vishnu.teja101.vt@gmail.com>
2025-09-04 18:03:24 +0530

    ascaksc

--- src/graphql/integrations/resolver.js
+++ src/graphql/integrations/resolver.js
@@ -97,7 +97,7 @@ export const IntegrationResolvers = {
             switch (integration.metaData.type) {
                 case "twilio":
                     const service = new TwilioService(integration.config.AccountSid, TWILIO_AUTH_TOKEN);
-                    await service.deauthorizeConnectApp(connectAppSid);
+                    await service.deauthorizeConnectApp(integration.config.AccountSid);
                     // remove channel connected to integration 
                     const deletedChannel = await Channel.findOneAndDelete({ "config.integration": id, business: context.user.business });
                     if (deletedChannel) {
