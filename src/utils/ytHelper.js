dbb64db0433fc0fdbbfaa5ae6cf4de7fdf174f45
viz-nu <vishnu.teja101.vt@gmail.com>
2025-10-02 23:57:31 +0530

    aslvk

--- src/utils/ytHelper.js
+++ src/utils/ytHelper.js
@@ -11,7 +11,7 @@ export const processYT = async (collectionId, urls, receiver, _id) => {
             let text = docs.map(ele => ele.pageContent).join('');
             try {
                 topics = await digest(text, url, collectionId, {}, topics, "text");
-                await Collection.updateOne({ _id: collectionId, "contents._id": _id }, { $push: { "contents.$.metaData.detailedReport": { success: true, url: url }, }, $addToSet: { topics: topics }, $set: { "contents.$.status": active } });
+                await Collection.updateOne({ _id: collectionId, "contents._id": _id }, { $push: { "contents.$.metaData.detailedReport": { success: true, url: url }, }, $addToSet: { topics: topics }, $set: { "contents.$.status": "active" } });
             } catch (error) {
                 console.error("Error during digesting:", error);
                 await Collection.updateOne({ _id: collectionId, "contents._id": _id }, { $push: { "contents.$.metaData.detailedReport": { success: false, url: url, error: error.message } }, $set: { "contents.$.status": "error" } });
