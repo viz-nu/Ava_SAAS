d2a920f71f3b29331a54c741ba65a92536d5ef71
viz-nu <vishnu.teja101.vt@gmail.com>
2025-10-02 23:43:51 +0530

    lkjbdvf

--- src/utils/socketIoClient.js
+++ src/utils/socketIoClient.js
@@ -1,14 +1,14 @@
 import io from 'socket.io-client';
-const SERVER_A_URL = 'wss://socketio.avakado.ai';
+const SERVER_A_URL = 'https://socketio.avakado.ai';
 export const sendMessageToRoom = (roomId, action, message, namespace) => {
     let socket;
     try {
         switch (namespace) {
             case 'admin':
-                socket = io(SERVER_A_URL + '/admin');
+                socket = io(SERVER_A_URL + '/admin', { transports: ['websocket'] });
                 break;
             case 'user':
-                socket = io(SERVER_A_URL + '/user');
+                socket = io(SERVER_A_URL + '/user', { transports: ['websocket'] });
                 break;
         }
         if (socket) socket.emit('trigger', { action, data: { roomId, message, namespace } });
