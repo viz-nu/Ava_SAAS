95d6a1c5e1088c5065776f2ba46eb89a543c74a9
viz-nu <vishnu.teja101.vt@gmail.com>
2025-12-10 21:30:02 +0530

    jksbvc

--- src/graphql/job/resolver.js
+++ src/graphql/job/resolver.js
@@ -110,7 +110,6 @@ export const jobResolvers = {
             return job;
         },
         makeAnOutboundCall: async (_, { number, channelId, PreContext = "", campaignId = null }, context, info) => {
-            console.log({ number, channelId, PreContext, campaignId })
             const channel = await Channel.findById(channelId).select({ config: 1, business: 1, type: 1 }).populate({ path: 'config.integration', select: { config: 1, secrets: 1 } }).lean();
             if (!channel) throw new GraphQLError("Channel not found", { extensions: { code: "CHANNEL_NOT_FOUND" } });
             const agentDetails = await AgentModel.findOne({ channels: channel._id }, "_id personalInfo.VoiceAgentSessionConfig");
@@ -131,7 +130,6 @@ export const jobResolvers = {
                     break;
                 case 'tataTele':
                     const tataTeleService = new TataTeleService(channel.config.integration.secrets.apiKey);
-                    console.log("tataTele:", { number, CallerId: channel.config.phoneNumber, customField: { conversationId: conversation._id, model: agentDetails.personalInfo.VoiceAgentSessionConfig.model } });
                     callDetails = await tataTeleService.outboundCallToFlow({ number, CallerId: channel.config.phoneNumber, customField: { conversationId: conversation._id, model: agentDetails.personalInfo.VoiceAgentSessionConfig.model } });
                     console.log("tataTele callDetails:", JSON.stringify(callDetails, null, 2));
                     break;
