d2130257471bba988606c1bdcf4e7034f9156046
viz-nu <vishnu.teja101.vt@gmail.com>
2026-04-14 00:47:16 +0530

    kjalsv

--- src/graphql/channels/resolver.js
+++ src/graphql/channels/resolver.js
@@ -17,23 +17,17 @@ const { wa_client_id, wa_client_secret, SERVER_URL, IG_CLIENT_Secret, IG_ClIENT_
 export const channelResolvers = {
     Query: {
         async getChannels(_, { limit = 10, page = 1, id, type, status }, context, info) {
-            try {
-                const requestedFields = graphqlFields(info, {}, { processArguments: false });
-                const { rootFields, populateFields } = getSelectFields(requestedFields.data);
-                console.log(rootFields, populateFields);
-                const filter = { business: context.user.business };
-                if (id) filter._id = id;
-                if (type) filter.type = type;
-                if (status) filter.status = status;
-                const channels = await Channel.find(filter).sort({ createdAt: -1 }).skip((page - 1) * limit).limit(limit).select(rootFields);
-                const totalDocuments = await Channel.countDocuments(filter);
-                if (populateFields?.business) await Business.populate(channels, { path: 'business', select: populateFields.business });
-                if (populateFields?.createdBy) await User.populate(channels, { path: 'createdBy', select: populateFields.createdBy });
-                return { data: channels, metaData: { page, limit, totalPages: Math.ceil(totalDocuments / limit), totalDocuments } };
-            } catch (error) {
-                console.log(error);
-            }
-
+            const requestedFields = graphqlFields(info, {}, { processArguments: false });
+            const { rootFields, populateFields } = getSelectFields(requestedFields.data);
+            const filter = { business: context.user.business };
+            if (id) filter._id = id;
+            if (type) filter.type = type;
+            if (status) filter.status = status;
+            const channels = await Channel.find(filter).sort({ createdAt: -1 }).skip((page - 1) * limit).limit(limit).select(rootFields);
+            const totalDocuments = await Channel.countDocuments(filter);
+            if (populateFields?.business) await Business.populate(channels, { path: 'business', select: populateFields.business });
+            if (populateFields?.createdBy) await User.populate(channels, { path: 'createdBy', select: populateFields.createdBy });
+            return { data: channels, metaData: { page, limit, totalPages: Math.ceil(totalDocuments / limit), totalDocuments } };
         },
     },
     Mutation: {
