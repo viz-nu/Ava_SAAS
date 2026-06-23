03525017f658999fd55257673b2b289dba9877ac
viz-nu <vishnu.teja101.vt@gmail.com>
2026-06-24 04:27:42 +0530

    setup contact lead

--- src/graphql/leads/resolver.js
+++ src/graphql/leads/resolver.js
@@ -200,8 +200,8 @@ export const leadResolvers = {
               const { whatsapp } = lead.contactDetails;
               toId = whatsapp?.find(entry => entry.isPrimary)?.handle ?? whatsapp?.[0]?.handle;
               platformMeta = {
-                accessToken: channel.apiAuthenticator.accessToken,
-                phone_number_id: channel.apiAuthenticator.phone_number_id,
+                accessToken: channel.apiAuthenticator.credentials.accessToken,
+                phone_number_id: channel.config.phone_number_id,
               };
               break;
           }
@@ -210,38 +210,33 @@ export const leadResolvers = {
           break;
       }
       // create a message and conenct it to a conversation
-      console.log(JSON.stringify([{
-        key: toId?.toString() ?? 'unknown',
-        value: JSON.stringify({ operation: 'sendMessage', toId, platformMeta, type, data, messageId: 'some message id'}),
-      }]));
-
-      // const mesasge = await Message.create({
-      //   conversation: conversationId,
-      //   business: context.user.business,
-      //   externalMessageId: toId?.toString() ?? 'unknown',
-      //   direction: 'outbound',
-      //   sender: {
-      //     type: 'user',
-      //     id: context.user._id,
-      //     name: context.user.name,
-      //     ref: context.user._id,
-      //     refModel: 'User',
-      //   },
-      //   type: type,
-      //   kind: 'message',
-      //   content: data,
-      //   statusTimeline: {
-      //     initiated: new Date(),
-      //   }
-      // });
-      // await sendKafkaMessage({
-      //   topic,
-      //   messages: [{
-      //     key: toId?.toString() ?? 'unknown',
-      //     value: JSON.stringify({ operation: 'sendMessage', toId, platformMeta, type, data, messageId: mesasge._id }),
-      //   }]
-      // });
-      return {_id: 'some message id'};
+      const messageDocument = await Message.create({
+        conversation: conversationId,
+        business: context.user.business,
+        externalMessageId: toId?.toString() ?? 'unknown',
+        direction: 'outbound',
+        sender: {
+          type: 'user',
+          id: context.user._id,
+          name: context.user.name,
+          ref: context.user._id,
+          refModel: 'User',
+        },
+        type: type,
+        kind: 'message',
+        content: data,
+        statusTimeline: {
+          initiated: new Date(),
+        }
+      });
+      await sendKafkaMessage({
+        topic,
+        messages: [{
+          key: toId?.toString() ?? 'unknown',
+          value: JSON.stringify({ operation: 'sendMessage', toId, platformMeta, type, data, messageId: messageDocument._id.toString() }),
+        }]
+      });
+      return messageDocument;
     },
 
     // ─── bulkCreateLeads ───────────────────────────────────────────────────────────
--- src/utils/kafka.js
+++ src/utils/kafka.js
@@ -3,12 +3,12 @@ import { Kafka, logLevel } from 'kafkajs'
 const brokers = ['98.84.157.16:9094']
 
 
-export async function sendKafkaMessage({ topic, message, acks = -1, }) {
+export async function sendKafkaMessage({ topic, messages, acks = -1, }) {
     const kafka = new Kafka({ clientId: 'avakado-producer', brokers, logLevel: logLevel.ERROR })
     const producer = kafka.producer()
     try {
         await producer.connect()
-        await producer.send({ topic, messages: [message], acks })// { key, value, headers, partition }
+        await producer.send({ topic, messages: messages, acks })// { key, value, headers, partition }
     } catch (error) {
         console.error('Error sending Kafka message:', error)
     } finally {
